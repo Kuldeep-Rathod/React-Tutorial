3a01725265c8b67522d357d137be84465b2a280a
Kuldeep Rathod <kuldiprathod375@gmail.com>
2024-02-26 12:44:56 +0530

    component and props

---
+++ 03component_props/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/card'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Card name="Kuldeep"/>
+      <Card name="shailesh"/>
+      <Card name="rahul"/>
+    </>
+  )
+}
+
+export default App
